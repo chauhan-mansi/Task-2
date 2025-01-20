440a84d7db2da237bea9d46b14452b0b4769ffd5
Mansi Chauhan <chauhanmansi2823@gmail.com>
2025-01-20 13:59:49 +0530

    used param in get by id

--- model/products/products.controller.js
+++ model/products/products.controller.js
@@ -23,20 +23,26 @@ exports.createProduct = async (req, res) => {
   }
 };
 
-// exports.getProduct = async (req, res) => {
-//   try {
-//     const products = await product.find();
-//     res.status(200).json({ success: true, data: products });
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: "Internal Server Error" });
-//   }
-// };
-
-exports.getProductById = async (req, res) => {
+exports.getProduct = async (req, res) => {
   try {
-    const products = await product.findById("678a2dc866dcd7a4f316206a");
+    const products = await product.find();
     res.status(200).json({ success: true, data: products });
   } catch (error) {
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
+};
+
+exports.getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const products = await product.findById(id);
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+  } catch (error) {}
 };
--- model/products/products.routes.js
+++ model/products/products.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createProduct } = require("./products.controller");
+const { createProduct, getProduct } = require("./products.controller");
 const { getProductById } = require("./products.controller");
+
 const router = express.Router();
 
 router.post("/", createProduct);
-router.get("/", getProductById);
+router.get("/:id", getProductById);
+router.get("/", getProduct);
 
 module.exports = router;
